06e0cce0954ae5c17cf87f16e65b837c86f5f149
Precious Adesakin <olabodeprecious2023@gmail.com>
2025-10-27 05:43:17 +0100

    Almost Done

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,15 @@ import './App.css';
 import { Navigate } from 'react-router-dom'
 import Home from "./assets/Home";
 import Details from "./assets/Details";
-import Navbar from './assets/Navbar';
-import Banner from './assets/Banner';
 import Contact from './assets/Contact'
-import Notfound from "./assets/Notfound";
 import AddProduct from './assets/AddProduct';
 import AdminDash from './assets/AdminDash';
 import Cart from './assets/Cart'
-// import Images from 'Images'
-// import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 import Dash from "./assets/Dash";
 import LogIn from "./assets/login";
 import Signup from "./assets/Signup";
 import { motion } from 'framer-motion';
-// import Cart from './assets/Cart';
 import Checkout from './assets/Checkout';
 import { CartcontentProvider } from './assets/Cartcontent';
 import CartNavLink from './assets/CartNavLink';
@@ -35,10 +29,6 @@ function App() {
   
     return(
       <>
-      {/* { <Navbar>Home| Log In| FormikForm</Navbar>} */}
-    
-
-
     <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/home" element={<Home/>}></Route>
@@ -52,9 +42,7 @@ function App() {
         <Route path="/admin/add" element={<AddProduct />} />
         <Route path="/checksucces" element={<Checkout />} />
         <Route path="/cart" element={<Cart />} />
-
     </Routes>
-
       </>
     );
     };
